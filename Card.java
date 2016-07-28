40abf1ab3600402368f3f9c2c5436ee6c00b2ee2
NadiyaQayyum07 <Nadiya.Qayyum@cdk.com>
2016-07-28 12:13:05 +0530

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by qayyumn on 7/28/2016.
+ */
+public class Card {
+    private 
+    private static final String[] suit = {"hearts", "spades", "diamonds", "clubs","joker"};
+    private static final String[] pip = {"Ace", "2", "3", "4", "5", "6", "7", "8",
+            "9", "10", "Jack", "Queen", "King"};
+
+
+
+
+            public static int compare(Card c){
+                if(this.)
+                return
+            }
+
+
+
+
+
+
+}
