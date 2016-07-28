ffadcfedb16d5e1f9aec37aa7c38c97c56fe3574
Himansh2294 <himanshu00238@gmail.com>
2016-07-28 12:25:56 +0530

    created a Card class

--- Card.java
+++ Card.java
@@ -1,25 +1,62 @@
-import java.util.ArrayList;
-
-/**
- * Created by qayyumn on 7/28/2016.
- */
-public class Card {
-    private 
-    private static final String[] suit = {"hearts", "spades", "diamonds", "clubs","joker"};
-    private static final String[] pip = {"Ace", "2", "3", "4", "5", "6", "7", "8",
-            "9", "10", "Jack", "Queen", "King"};
+public class Card{
+	private int cardValue; 
+	private String cardSuit;
+	private String stringValue;
+	static final String[] SUIT = {"Clubs", "Diamonds", "Hearts", "Spades", "Joker"}; 
+	static final String[] PIP =  {"Ace","Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}; 
 
 
 
+	public Card (int cardS, int cardV){
 
-            public static int compare(Card c){
-                if(this.)
-                return
-            }
-
-
+    		cardSuit = SUIT[cardS]; 
+    		stringValue = VALUE[cardV];
+	}
 
 
+	public String getCardSuit(){
+        	return cardSuit;
+	}
 
 
+	public String getValue(){
+    		return stringValue;
+	}
+
+	public String toString() { 
+    		return String.format("%s of %s", stringValue, cardSuit); 
+	} 
+
+public int compareTo(Card otherCard) {       
+        //compare card
+        int cardDiff = card.compareToIgnoreCase(otherCard.card);
+        if(cardDiff != 0){
+            return cardDiff;
+        }else{
+		return 0;
+		}
+	}
+public void acceptData(){
+	Card card = new Card();
+	
+    }
+  
 }
+/*
+public int compareTo(Card card) {
+    if (this.SUIT < card.SUIT) {
+      return -1;
+    } else if (this.SUIT > card.SUIT) {
+      return 1;
+    } else {
+      // suit is identical: compare number
+      if (this. < card.PIP) {
+        return -1;
+      } else if (this.PIP > card.PIP) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+}*?
